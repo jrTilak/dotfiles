909d77b3d43e068de2dc1f28f2bd75362f4baba2
jrtilak <contact@jrtilak.dev>
2026-03-25 13:17:49 +0545

    bare repo

--- .config/nvim/lua/config/keymaps.lua
+++
@@ -1,6 +0,0 @@
--- Toggle Markdown preview in floating window
-vim.keymap.set("n", "<leader>mt", ":Glow<CR>", { desc = "Markdown Terminal Preview" })
-
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here

5c0a5522322cbf0080d30ca6a0b438550ab7a95b
jrtilak <contact@jrtilak.dev>
2026-04-04 20:32:02 +0545

    update nvm

---
+++ .config/nvim/lua/plugins/mini-icons.lua
@@ -0,0 +1,17 @@
+return {
+  "nvim-mini/mini.icons",
+  opts = {
+    file = {
+      -- vite config
+      ["vite.config.js"] = { glyph = "\u{e8d7}", hl = "MiniIconsYellow" },
+      ["vite.config.ts"] = { glyph = "\u{e8d7}", hl = "MiniIconsYellow" },
+      ["vite.config.mjs"] = { glyph = "\u{e8d7}", hl = "MiniIconsYellow" },
+      ["vite.config.mts"] = { glyph = "\u{e8d7}", hl = "MiniIconsYellow" },
+      ["vite.config.cjs"] = { glyph = "\u{e8d7}", hl = "MiniIconsYellow" },
+
+      -- LICENSE
+      ["LICENSE"] = { glyph = "\u{f0219}", hl = "MiniIconsYellow" },
+      ["LICENCE"] = { glyph = "\u{f0219}", hl = "MiniIconsYellow" },
+    },
+  },
+}

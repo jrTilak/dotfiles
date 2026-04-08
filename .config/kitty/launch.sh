0fd75c1fd17ba3f622db1c30857d8b068618001e
jrtilak <contact@jrtilak.dev>
2026-04-08 18:24:56 +0545

    update

--- .config/kitty/launch.sh
+++ .config/kitty/launch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-kitty &
+/home/jrtilak/.local/kitty.app/bin/kitty &
 sleep 0.5
 ID=$(xdotool search --sync --class kitty | tail -1)
 xprop -id $ID -f _KDE_NET_WM_BLUR_BEHIND_REGION 32c -set _KDE_NET_WM_BLUR_BEHIND_REGION 0

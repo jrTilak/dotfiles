65456c3333e8a831e24e80b406eeb5e67be73a3d
jrtilak <contact@jrtilak.dev>
2026-08-16 23:55:14 +0545

    fix nvim config

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,6 +1,8 @@
-require("config.remote_clipboard").setup()
--- Options are automatically loaded before lazy.nvim startup
+-- Options are automatically loaded before lazy.nvim startup.
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+require("config.remote_clipboard").setup()
+
 vim.opt.relativenumber = true
+vim.g.autoformat = false
--- nvim/.config/nvim/lua/plugins/theme.lua
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -1,1 +1,18 @@
-../../../../.local/state/omarchy/current/theme/neovim.lua
+local theme_path = vim.fn.expand("~/.local/state/omarchy/current/theme/neovim.lua")
+
+if vim.fn.filereadable(theme_path) == 1 then
+  return dofile(theme_path)
+end
+
+return {
+  {
+    "folke/tokyonight.nvim",
+    priority = 1000,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight-night",
+    },
+  },
+}
